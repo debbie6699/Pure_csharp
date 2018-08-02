48c0d3f2076a4a6a8edf64555bde0e26af0f6a0e
debbie <deborah7229@gmail.com>
2018-08-02 16:57:08 +0800

    1070802

--- ConsoleApp2_排列順序/ConsoleApp2_排列順序/Program.cs
+++ ConsoleApp2_排列順序/ConsoleApp2_排列順序/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp2_排列順序
     {
         static void Main(string[] args)
         {
+            double[] A;
+            A = new double[] { 4.2, 0.6, 5.3, 11.2, 3.8 };
+            Console.WriteLine();
+
+            for(int t=0; t < A.Length;t++)
+            {
+                Console.Write(A[t]+", ");
+            }
+            Console.WriteLine();
+
+
+
         }
     }
 }
